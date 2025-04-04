f95ac87d935541547a7bcbd910258be75906e39b
James Morton <jamesmorton@Jamess-MBP.localdomain>
2025-04-04 15:07:47 +0100

    added the form in with the relevant css and javscript to go along with it

--- JS/script.js
+++ JS/script.js
@@ -5,4 +5,12 @@ function myFunction() {
     } else {
       x.style.display = "none";
     }
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
   }
